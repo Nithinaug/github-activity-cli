d389863e4dab703761671ca47b3530997e0fc3b2
Nithinaug <nithinaug@gmail.com>
2026-05-10 19:51:20 +0530

    updated types.go

--- types.go
+++ types.go
@@ -1,11 +1,49 @@
 package main
 
 type GitHubEvent struct {
-	Type string `json:"type"`
+	Type      string  `json:"type"`
+	Repo      Repo    `json:"repo"`
+	Payload   Payload `json:"payload"`
+	CreatedAt string  `json:"created_at"`
+}
 
-	Repo struct {
-		Name string `json:"name"`
-	} `json:"repo"`
+type Repo struct {
+	Name string `json:"name"`
+}
+type Payload struct {
+	// PushEvent
+	Commits []Commit `json:"commits"`
+	Size    int      `json:"size"`
+	Action  string   `json:"action"`
 
-	CreatedAt string `json:"created_at"`
+	Issue *Issue `json:"issue"`
+
+	PullRequest *PullRequest `json:"pull_request"`
+	RefType     string       `json:"ref_type"`
+	Ref         string       `json:"ref"`
+
+	Forkee  *Forkee  `json:"forkee"`
+	Comment *Comment `json:"comment"`
+}
+
+type Commit struct {
+	Message string `json:"message"`
+}
+
+type Issue struct {
+	Number int    `json:"number"`
+	Title  string `json:"title"`
+}
+
+type PullRequest struct {
+	Number int    `json:"number"`
+	Title  string `json:"title"`
+}
+
+type Forkee struct {
+	FullName string `json:"full_name"`
+}
+
+type Comment struct {
+	Body string `json:"body"`
 }
